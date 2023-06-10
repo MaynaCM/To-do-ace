32501d2f110575a4cb307b3a1cc1f47913de6fcc
MaynaCM <maynnamartins@gmail.com>
2023-06-10 14:10:37 -0300

    Database configuration

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
-const PORT = process.env.PORT ?? 8000
+const PORT = process.env.PORT ?? 8080
 const express = require('express')
 const app = express()
+const pool = require('./db')
 
 // get table todos(to-do's)
-app.get('/todos', (req, res) => {
+app.get('/todos', async (req, res) => {
+    const userEmail = "a@test.com"
+    
     try{
-        await
+      const todos =  await pool.query('SELECT * FROM todos WHERE user_emai; = $1', [userEmail])
+      res.json(todos.rows)
     } catch(err) {
-        console.log(error)
+        console.log(err)
     }
 })
 
-app.listen(PORT, ( ) => `Server running on PORT ${PORT}`)
+app.listen(PORT, ( ) => console.log(`Server running on PORT ${PORT}`))
